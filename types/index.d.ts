066b324629b6e8c957511fb31744d0196759d3a9
Michael Timmons <michael.timmons@protonmail.com>
2023-08-08 18:52:19 -0400

    Fix type

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,13 +13,6 @@ export type MainNavItem = NavItem
 
 export type SiteConfig = {
     name: string
-    description: string
-    url: string
-    ogImage: string
-    links: {
-        twitter: string
-        github: string
-    }
 }
 
 export type DashboardConfig = {
